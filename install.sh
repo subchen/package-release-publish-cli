ebcc107f9f4c5e512de2ae669f3919a360e07726
Guoqiang Chen <subchen@gmail.com>
2018-03-16 12:05:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
-VERSION=0.1.0
+tag=v0.1.0
 
+os=$(uname -s | tr '[A-Z]' '[a-z]')
 
+curl -fSL https://github.com/subchen/publish-toolset/releases/download/$tag/publish-toolset-$os.zip -o publish-toolset.zip
+unzip publish-toolset.zip -d /usr/local/bin/
+rm -f publish-toolset.zip
