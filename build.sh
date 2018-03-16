d6d39ed79e8dada2bb8eb033b0f7a7e288801025
Guoqiang Chen <subchen@gmail.com>
2018-03-16 11:57:50 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,17 +9,37 @@ COMPONENTS="
     github-release-upload
 "
 
+echo "Building publish-toolset ..."
+
 for app in $COMPONENTS; do
-    cd $CWD/$app && make release
+    cd $CWD/$app && make build
+done
+    
+if [ -z "$TARVIS_TAG" ]; then
+    exit 0
+fi
+
+echo "Uploading files into github release: $TARVIS_TAG ..."
+
+version="${TARVIS_TAG}"
+github_release_upload="$CWD/_releases/linux/github_release_upload"
+sha256sum_files="$CWD/_releases/linux/sha256sum_files"
+
+rm -rf   $CWD/_releases/
+mkdir -p $CWD/_releases/linux
+mkdir -p $CWD/_releases/darwin
+mkdir -p $CWD/_releases/windows
+
+for app in $COMPONENTS; do
+    mv -f $CWD/$app/_releases/*-linux-*   $CWD/_releases/linux/$app
+    mv -f $CWD/$app/_releases/*-darwin-*  $CWD/_releases/darwin/$app
+    mv -f $CWD/$app/_releases/*-windows-* $CWD/_releases/windows/$app.exe
 done
 
-github_release_upload=$(ls $CWD/github-release-upload/_releases/github-release-upload-*-linux-amd64)
+zip $CWD/_releases/publish-toolset-$version-linux-amd64.zip   $CWD/_releases/linux/*
+zip $CWD/_releases/publish-toolset-$version-darwin-amd64.zip  $CWD/_releases/darwin/*
+zip $CWD/_releases/publish-toolset-$version-windows-amd64.zip $CWD/_releases/windows/*
 
-if [ -n "$TARVIS_TAG" ]; then
-    echo "Uploading into github release: $TARVIS_TAG ..."
+"$sha256sum_files" $CWD/_releases/*.zip
 
-    for app in $COMPONENTS; do
-        "$github_release_upload" "$CWD/$app/_releases"
-    done
-
-fi
+"$github_release_upload" "$CWD/$app/_releases"
